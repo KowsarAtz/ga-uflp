d580101d7117779e0720789a36020566548012f9
Kowsar Atazadeh <kowsar.atazadeh@gmail.com>
2020-04-25 23:47:19 +0430

    update test procedure

--- test.py
+++ test.py
@@ -1,25 +1,30 @@
 from UFLPGeneticProblem import UFLPGeneticProblem
 
-ITERATIONS = 20
-
 datasets = []
 # datasets += ['70/cap71','70/cap72','70/cap73','70/cap74']
 # datasets += ['100/cap101', '100/cap102', '100/cap103', '100/cap104']
-datasets += ['130/cap131', '130/cap132', '130/cap133', '130/cap134']
+# datasets += ['130/cap131', '130/cap132', '130/cap133', '130/cap134']
 # datasets += ['a-c/capa', 'a-c/capb', 'a-c/capc']
+datasets += ['a-c/capa']
 
 
 OPTIMAL = 'optimal'
 BELOWp2 = 'below 0.2'
 BELOW1 = 'below 1'
 ABOVE1 = 'above 1'
+REACHED = 'reached scores'
+FAILED = 'failed scores'
 lastFaild = ''
+ITERATIONS = 5
 
 reached = {}
 for dataset in datasets:
-    reached[dataset] = {OPTIMAL: 0, BELOWp2: 0, BELOW1: 0, ABOVE1: 0}
+    reached[dataset] = {OPTIMAL: 0, BELOWp2: 0, BELOW1: 0, ABOVE1: 0,
+                        REACHED: [], FAILED: []}
 
 # problems = []
+failedScores = []
+reachedScores = []
 totalReached = 0
 totalFailed = 0
 total = ITERATIONS * len(datasets)
@@ -30,10 +35,11 @@ for i in range(ITERATIONS):
                                      orlibDataset = dataset,
                                      maxGenerations = 2000,
                                      nRepeatParams = (2,0.5), 
-                                     mutationRate = 0.01,
+                                     mutationRate = 0.005,
                                      crossoverMaskRate = 0.3,
                                      eliteFraction = 2/3,
-                                     printSummary = False
+                                     printSummary = True,
+                                     populationSize = 150
                                      )
         
         problem.run()
@@ -42,18 +48,20 @@ for i in range(ITERATIONS):
         if error == 0:
             totalReached += 1
             reached[dataset][OPTIMAL] += 1
-        elif error < 0.2:
-            reached[dataset][BELOWp2] += 1
-        elif error < 1:
-            reached[dataset][BELOW1] += 1
+            reached[dataset][REACHED] += [problem.score]
         else:
-            reached[dataset][ABOVE1] += 1
-        
-        if False in problem.compareToOptimal:
             totalFailed += 1
             lastFaild = dataset
+            reached[dataset][FAILED] += [problem.score]
+            if error < 0.2:
+                reached[dataset][BELOWp2] += 1
+            elif error < 1:
+                reached[dataset][BELOW1] += 1
+            else:
+                reached[dataset][ABOVE1] += 1
+        
         # problems += [(dataset, i, problem)]
-        print('\rTotal Reached %d/%d  Failed %d (last failed %s)                     ' \
-              % (totalReached, total, totalFailed, lastFaild), end = '')
+        # print('\rTotal Reached %d/%d  Failed %d (last failed %s)                     ' \
+        #       % (totalReached, total, totalFailed, lastFaild), end = '')
 
     

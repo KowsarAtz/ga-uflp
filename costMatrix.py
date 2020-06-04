9b2e79f5b91a15f97fc929ba32b5d92f1d4343a3
K-Atz <kowsar.atazadeh@gmail.com>
2020-06-04 17:49:09 +0430

    plot assigned facilities

--- costMatrix.py
+++ costMatrix.py
@@ -11,8 +11,40 @@ columnsCount = int(inputFile.readline())
 costMatrix = np.array([int(i) for i in inputFile.readlines()]).reshape(rowsCount, columnsCount)
 nodes = pd.read_csv('./reports/phaseTwoPartitioning/dataset/pa/csv/nodes.csv').iloc[:, 1:3].values
 
-samples = nodes[sample(range(nodes.shape[0]), SAMPLES_NO)]
+sampleIndices = sample(range(nodes.shape[0]), SAMPLES_NO)
+facilityToCustomerCost = costMatrix[sampleIndices]
+facilityToCustomerCost = facilityToCustomerCost[:, [i for i in range(costMatrix.shape[1]) if i not in sampleIndices]]
 
-plt.scatter(nodes[:,0], nodes[:,1], marker='.', linewidth=1)
-plt.scatter(samples[:,0], samples[:,1], marker='.', linewidth=1, color='red')
-plt.show()
+facilityNodes = nodes[sampleIndices]
+customerNodes = nodes[[i for i in range(costMatrix.shape[1]) if i not in sampleIndices]]
+
+from UFLPGeneticProblem import UFLPGeneticProblem
+
+ga = UFLPGeneticProblem(
+    np.zeros((SAMPLES_NO, ), np.float64), #potentialSitesFixedCosts
+    facilityToCustomerCost,
+    mutationRate = 0.01,
+    crossoverMaskRate = 0.4,
+    eliteFraction = 1/3,
+    populationSize = 150,
+    cacheParam = 50,
+    maxRank = 2.5,
+    minRank = 0.712,
+    maxGenerations = 2000,
+    nRepeat = None,
+    printProgress = True
+)
+
+ga.run()
+bestIndividual = ga.population[0]
+bestPlan = ga.bestIndividualPlan()
+
+establishedFacilities = np.unique(ga.bestPlan)
+for facility in establishedFacilities:
+    coveredNodes = customerNodes[np.where(np.array(ga.bestPlan) == facility)]
+    plt.scatter(nodes[:,0], nodes[:,1], marker='.', linewidth=1)
+    plt.scatter(coveredNodes[:,0], coveredNodes[:,1], marker='.', linewidth=1, color='red')
+    plt.scatter(facilityNodes[facility,0], facilityNodes[facility,1], marker='x', color='green')
+    plt.show()
+    
+import seaborn as sns
